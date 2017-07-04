7fc792ed0f364b25d8e6c968d521c599cc0a9095
Kanaga2 <priyakanaga112@gmail.com>
2017-07-04 18:32:01 +0530

    Create Contains.java

---
+++ Contains.java
@@ -0,0 +1,14 @@
+# Contains
+import java.io.*;
+import java.util.*;
+public class Contains
+{
+public static void main(String args[])
+{
+BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+String str1="hello world";
+String str2="sree";
+boolen c=str.contains(str2);
+System.out.println(c);
+}
+}
